960426fb67eed9b53edc7ed664c91f1f41d144a7
Sophie Shekriladze <55558347+Sophie-Shekriladze@users.noreply.github.com>
2023-03-20 21:53:13 +0400

    Add

--- script.js
+++ script.js
@@ -3,29 +3,20 @@
 //burger bar
 let navigation = document.getElementById("navBar");
 let burgerBar = document.getElementById("burgerBar");
-
+let lineOne = document.getElementById("line1");
+let lineTwo = document.getElementById("line2");
+let lineThree = document.getElementById("line3");
 
 burgerBar.addEventListener("click", function () {
   navigation.classList.toggle("active");
+  lineOne.classList.toggle("oneLine");
+  lineTwo.classList.toggle("twoLine");
+  lineThree.classList.toggle("threeLine");
 });
 
-
-
-
-
-
-
-
-
-
-
-
 let button = document.querySelector(".btn-add");
-
 let inputField = document.querySelector(".input");
-
 let ulItem = document.querySelector(".ul-item");
-
 let formElement = document.querySelector(".form-block");
 
 formElement.addEventListener("submit", function (event) {
@@ -50,4 +41,8 @@ formElement.addEventListener("submit", function (event) {
 
 document.querySelector(".clearall").addEventListener("click", function () {
   ulItem.innerHTML = " ";
+});
+
+document.getElementById("info").addEventListener("focus", function () {
+  info.style.outline = "none";
 });
